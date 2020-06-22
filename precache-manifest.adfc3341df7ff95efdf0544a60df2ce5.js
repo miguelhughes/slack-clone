05b1b72ad3b4157010b5873d1c2fcb5a220ff888
Miguel Hughes <miguel.hughes@gmail.com>
2020-06-22 10:45:21 -0300

    Updates

---
+++ precache-manifest.adfc3341df7ff95efdf0544a60df2ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6613547c37a72d6a91b627eca36e94b",
+    "url": "/slack-clone/index.html"
+  },
+  {
+    "revision": "de1ff4456893c2618cfc",
+    "url": "/slack-clone/static/css/main.3e475463.chunk.css"
+  },
+  {
+    "revision": "aed198fb7d59ff5b55f1",
+    "url": "/slack-clone/static/js/2.6dd90974.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slack-clone/static/js/2.6dd90974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1ff4456893c2618cfc",
+    "url": "/slack-clone/static/js/main.6154aef1.chunk.js"
+  },
+  {
+    "revision": "e7eaad4611cc91b352f7",
+    "url": "/slack-clone/static/js/runtime-main.3bdfda92.js"
+  }
+]);
